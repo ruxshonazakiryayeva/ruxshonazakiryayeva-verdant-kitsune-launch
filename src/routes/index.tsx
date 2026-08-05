f5b5bf39a780f7c343a06cb45e35f437addea2da
Ruxshona <133767416+ruxshonazakiryayeva@users.noreply.github.com>
2026-08-05 14:37:08 +0500

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -354,7 +354,7 @@ const CATALOG: CatItem[] = [
     tagKey: "filter.wedding",
     title: "Golden Vows",
     descKey: "cat.item2",
-    href: "",
+    href: "https://wedding-invitation-1-jasur-nilufar.vercel.app/",
     img: "https://storage.googleapis.com/gpt-engineer-file-uploads/attachments/og-images/8d8f0a38-d2b5-4e0d-8af2-06948233b80f",
   },
   {

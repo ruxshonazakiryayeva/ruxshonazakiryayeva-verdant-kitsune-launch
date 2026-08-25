431d8735a2c575e3ca5f7ee89ac1c3a5948bf8a1
Ruxshona <133767416+ruxshonazakiryayeva@users.noreply.github.com>
2026-08-25 10:00:49 +0500

    Create paymentConfig.ts

---
+++ src/lib/paymentConfig.ts
@@ -0,0 +1,7 @@
+// Bu qiymatlar sirli emas, saytda ko'rinadi. O'zingizga moslang.
+export const PAYMENT_CONFIG = {
+  cardNumber: "8600 0000 0000 0000",
+  cardOwner: "Ruxshona Zakiryayeva",
+  adminPhone: "+998 93 905 13 17",
+  adminTelegram: "https://t.me/erkaqizgina1317",
+};
